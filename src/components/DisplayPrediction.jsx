3fd2ed4007dbec0fe7324a27742dacdfef9d98f5
Jonathan VARELA <jonathan.varela29@gmail.com>
2020-03-02 19:28:07 +0100

    add spinner

--- src/components/DisplayPrediction.jsx
+++ src/components/DisplayPrediction.jsx
@@ -2,19 +2,26 @@ import * as React from "react";
 import firebase from "firebase"
 import {useEffect} from "react";
 import Iframe from "react-iframe";
+import {useState} from "react";
+import Loader from 'react-loader-spinner'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 const DisplayPrediction: React.Component = () => {
 
     const database = firebase.database().ref().child('hasChangedMockup');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         database.on(
             'value',
             (hasChangedHtmlDataSnap) => {
+                setLoading(true);
                 if (hasChangedHtmlDataSnap.val()) {
-                    database.set(false).then(
+                    database.set(false).then(() => {
                         document.getElementById(`predicationIframe`).src =
-                            document.getElementById('predicationIframe').src);
+                            document.getElementById('predicationIframe').src;
+                        setLoading(false);
+                    })
                 }
             }
         )
@@ -22,6 +29,24 @@ const DisplayPrediction: React.Component = () => {
 
     return (
         <div>
+            {
+                loading === false
+                    ? <div style={{
+                        position: "absolute",
+                        marginLeft: (window.innerWidth - 124) / 2,
+                        marginTop: (window.innerHeight - 124) / 2
+                    }}>
+                        <Loader
+                            type="Grid"
+                            color="blue"
+                            height={120}
+                            width={120}
+
+                        />
+                    </div>
+                    : ''
+            }
+
             <Iframe
                 url="http://localhost:5000/api/predict"
                 id={"predicationIframe"}
